bc5f304e67825fc89b90ea187fbe47c5987b774b
ItsmeAlza <alizamahat833@gmail.com>
2025-04-23 12:34:10 +0545

    Create Referral dashboard link

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { getSyncLifecycle } from '@openmrs/esm-framework';
+import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
+import { dashboardMeta } from './dashboard.meta';
 
 const moduleName = '@trigonal/esm-referral-app';
 
@@ -7,3 +10,12 @@ const options = {
 };
 
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+
+export const referralDashboardLink =
+  getSyncLifecycle(
+    createDashboardLink({
+      ...dashboardMeta,
+      moduleName,
+    }),
+    options,
+  );
